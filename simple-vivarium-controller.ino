04fc45a0503558cd69d3bf9cc35cc54ee0cb9692
psdaito <pstalonczyk@gmail.com>
2016-03-31 10:03:42 -0500

    Update simple-vivarium-controller.ino

--- simple-vivarium-controller.ino
+++ simple-vivarium-controller.ino
@@ -195,7 +195,7 @@ void loop()
 	  updateTimeDate(false);//passing false results in updating screen only if time or date has changed
 	}
   }
-  //menuActions();  uncomment for live testing
+  menuActions(); 
   Alarm.delay(10);
 }
 
@@ -232,7 +232,7 @@ void SaveTime()
   RTC.set(saveTime);
   if(dispScreen == 1){
 	  updateTimeDate(true);
-	}
+  }
 }
 time_t tmConvert_t(int YYYY, byte MM, byte DD, byte hh, byte mm, byte ss)
 {
